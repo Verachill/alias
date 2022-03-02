9e609f1e930ebaab66a8d43a4175cd3b7d5a04ac
Ykrop3000 <grisha.andreev.03@gmail.com>
2022-03-02 19:15:02 +0300

    add save in localstotage color mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import { wordType } from './types'
-import { fetchWordsThemesAll, fetchRandomWords } from './api/api'
+import React, { useContext } from 'react'
 import StartPage from './pages/StartPage'
 // ------------------------
 import { ThemeProvider } from 'styled-components'
 import { GameProvider } from './contexts/GameContext'
-import { ConfigProvider } from './contexts/ConfigContext'
+import { Context as ConfigContext } from './contexts/ConfigContext'
 // ------------------------
 
 import { darkTheme, lightTheme } from './colorscheme'
 
 export default function App() {
-  // const [, setData] = useState<wordType[]>([])
-  // const [, setData1] = useState<wordType[]>([])
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const dataFetched = await fetchRandomWords({ limit: 10 })
-  //     setData(dataFetched)
-  //   }
-  //   getData()
-  // }, [setData])
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const dataFetched = await fetchWordsThemesAll({
-  //       themes: 'test',
-  //       limit: 10,
-  //     })
-  //     setData1(dataFetched)
-  //   }
-  //   getData()
-  // }, [setData1])
-
-  const toggleTheme = () => {
-    window.localStorage.setItem('isDark', JSON.stringify(!isDark))
-    setIsDark(!isDark)
-  }
-
-  const [isDark, setIsDark] = React.useState<boolean>(
-    JSON.parse(window.localStorage.getItem('isDark') || 'false')
-  )
+  const config = useContext(ConfigContext)
 
   return (
-    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-      <ConfigProvider>
-        <GameProvider>
-          <StartPage toggleTheme={() => toggleTheme()} />
-        </GameProvider>
-      </ConfigProvider>
+    <ThemeProvider theme={config?.state.isDark ? darkTheme : lightTheme}>
+      <GameProvider>
+        <StartPage
+          toggleTheme={() => config?.dispatch({ type: 'toggle_isDark' })}
+        />
+      </GameProvider>
     </ThemeProvider>
   )
 }
--- src/contexts/ConfigContext.tsx
+++ src/contexts/ConfigContext.tsx
@@ -1,9 +1,9 @@
 import React, { createContext, useReducer } from 'react'
 
-type Action = { type: 'toggle_sound' }
+type Action = { type: 'toggle_sound' } | { type: 'toggle_isDark' }
 
 type Dispatch = (action: Action) => void
-type State = { sound: boolean }
+type State = { sound: boolean; isDark: boolean }
 
 type ProviderProps = { children: React.ReactNode }
 
@@ -14,7 +14,12 @@ const Context = createContext<{ state: State; dispatch: Dispatch } | undefined>(
 function configReducer(state: State, action: Action) {
   switch (action.type) {
     case 'toggle_sound': {
+      window.localStorage.setItem('sound', JSON.stringify(!state.sound))
       return { ...state, sound: !state.sound }
+    }
+    case 'toggle_isDark': {
+      window.localStorage.setItem('isDark', JSON.stringify(!state.isDark))
+      return { ...state, isDark: !state.isDark }
     }
     default: {
       throw new Error(`Unhandled action type`)
@@ -22,8 +27,13 @@ function configReducer(state: State, action: Action) {
   }
 }
 
+const loadConfigs = {
+  sound: JSON.parse(window.localStorage.getItem('sound') || 'false'),
+  isDark: JSON.parse(window.localStorage.getItem('isDark') || 'false'),
+}
+
 function ConfigProvider({ children }: ProviderProps) {
-  const [state, dispatch] = useReducer(configReducer, { sound: true })
+  const [state, dispatch] = useReducer(configReducer, loadConfigs)
 
   const value = { state, dispatch }
   return <Context.Provider value={value}>{children}</Context.Provider>
